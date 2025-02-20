4ef9364fb7758d72271ab82313fc180dc41883af
Ildegar <illianclase12345678@gmail.com>
2025-02-20 18:14:17 +0000

    Fetch funcional

--- src/components/llamadaApi.jsx
+++ src/components/llamadaApi.jsx
@@ -1,25 +1,39 @@
-export default function llamadaApi() {
-    const [products, setProducts] = useState([]);
+import React, { useEffect, useState } from 'react';
+
+const LlamadaApi = () => {
+    const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const response = await fetch(`http://127.0.0.1:8000/api/associations`, { credentials: "include" });
-                setProducts(response.data.data);
+        fetch('https://yeray.informaticamajada.es/api/users')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setData(data);
                 setLoading(false);
-            } catch (err) {
-                setError(err.message);
+            })
+            .catch((error) => {
+                setError(error);
                 setLoading(false);
-            }
-        };
-
-        fetchProducts();
+            });
     }, []);
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error}</div>;
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-    return (console.log(products));
-}
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return (
+        console.log(data.data)
+    );
+};
+
+export default LlamadaApi;
