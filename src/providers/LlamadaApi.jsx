cdd13c5cc9efeccbf337ebb94588c18b58099a8a
Ildegar <illianclase12345678@gmail.com>
2025-02-22 03:55:05 +0000

    Trae datos y los guarda en local

--- src/providers/LlamadaApi.jsx
+++ src/providers/LlamadaApi.jsx
@@ -1,42 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 
-export default function LlamadaApi({ table }) {
-    const [data, setData] = useState(null);
+export default function LlamadaApi(datoBuscar) {
+    const [datosApi, setDatosApi] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+
+    const obtenerDatos = useCallback(async () => {
+        const nombreItem = `${datoBuscar}Data`;
+        const datosExistentes = localStorage.getItem(nombreItem);
+
+        if (datosExistentes) {
+            setDatosApi(JSON.parse(datosExistentes));
+            setLoading(false);
+
+        } else {
+            try {
+                const llamadaApi = await axios.get(`https://yeray.informaticamajada.es/api/${datoBuscar}`);
+                localStorage.setItem(nombreItem, JSON.stringify(llamadaApi.data));
+                setDatosApi(llamadaApi.data);
+                setLoading(false);
+
+            } catch (error) {
+                console.log('Fallo al llamar los datos:', error);
+                setLoading(false);
+            }
+        }
+    }, []);
 
     useEffect(() => {
-        const fetchData = async () => {
-            const cachedData = localStorage.getItem('associationsData');
-            if (cachedData) {
-                setData(JSON.parse(cachedData));
-                setLoading(false);
-            } else {
-                try {
-                    const response = await axios.get(`https://yeray.informaticamajada.es/api/${table}`);
-                    localStorage.setItem('associationsData', JSON.stringify(response.data));
-                    setData(response.data);
-                    setLoading(false);
-                } catch (error) {
-                    setError(error);
-                    setLoading(false);
-                }
-            }
-        };
-
-        fetchData();
-    }, [table]);
+        obtenerDatos();
+    }, [obtenerDatos]);
 
     if (loading) {
         return <div>Loading...</div>;
     }
 
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
-
-    return (
-        console.log(localStorage.getItem('associationsData'))
-    );
+    return (console.log(datosApi.data));
 };
